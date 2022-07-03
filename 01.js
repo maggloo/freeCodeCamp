99efb6ca1529100bff4f27d0a5942273f4fcbaf4
Margarita Privalko <87192791+maggloo@users.noreply.github.com>
2022-07-03 16:47:01 +0300

    Update 01.js

--- 01.js
+++ 01.js
@@ -3,4 +3,4 @@ let firstLetterOfLastName = "";
 const lastName = "Lovelace";
 
 // Only change code below this line
-firstLetterOfLastName = lastName; // Change this line
+firstLetterOfLastName = lastName[0]; // Change this line
